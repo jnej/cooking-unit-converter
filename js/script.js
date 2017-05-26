b138da255b215f0fcb6e31af6d596939c9c1bcce
Amanda Johnson <amandajnej@gmail.com>
2017-05-26 11:52:00 +0100

    Add initial JS and HTML code

---
+++ js/script.js
@@ -0,0 +1,19 @@
+//oz converted to ml//
+
+var volume = function(ml){return ml*29.57;};
+
+var ml = parseFloat(prompt("Enter volume in fluid oz"));
+
+var result = volume(ml);
+
+alert(result);
+
+//kg converted to lb//
+
+var weight = function(kg){return kg*2.2;};
+
+var lb = parseFloat(prompt("Enter weight in kg"));
+
+var result = weight(lb);
+
+alert(result);

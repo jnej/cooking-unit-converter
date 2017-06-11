a69a5c00254e50f76e7a7b83fc868dca3b743832
Amanda Johnson <amandajnej@gmail.com>
2017-06-11 21:43:59 +0100

    add user interface.

--- js/script.js
+++ js/script.js
@@ -1,19 +1,42 @@
 //oz converted to ml//
 
-var volume = function(ml){return ml*29.57;};
+var volume = function(oz) {
+  return oz*29.57;
+};
 
-var ml = parseFloat(prompt("Enter volume in fluid oz"));
+//user interface//
 
-var result = volume(ml);
+$(document).ready(function() {
+  $("#millilitre").submit(function(event) {
+    var oz = parseFloat($("#oz").val());
+    var result = volume(oz);
 
-alert(result);
+    $(".output").text(result + " ml");
+
+    event.preventDefault();
+  });
+});
 
 //kg converted to lb//
 
-var weight = function(kg){return kg*2.2;};
+var weight = function(kg)
+{
+  return kg*2.2;
+};
 
-var lb = parseFloat(prompt("Enter weight in kg"));
+/*var lb = parseFloat(prompt("Enter weight in kg"));
 
 var result = weight(lb);
 
-alert(result);
+alert(result);*/
+
+$(document).ready(function() {
+  $("#lbs").submit(function(event) {
+    var kg = parseFloat($("#kg").val());
+    var result = weight(kg);
+
+    $(".output1").text(result + " lbs");
+
+    event.preventDefault();
+  })
+})
